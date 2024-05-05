e2f2c914d2ed61bbe60e4ade2df53e2ccfed98d0
Guido <guido211@hotmail.it>
2024-05-05 16:00:13 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -4,7 +4,7 @@ def buildJar() {
 } 
 
 def buildImage() {
-    echo "building the docker image..."
+    echo "building the docker age..."
    // withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
      //   sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
      //   sh "echo $PASS | docker login -u $USER --password-stdin"
